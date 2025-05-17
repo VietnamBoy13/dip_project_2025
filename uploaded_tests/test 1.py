7d74ba761a831d5cb73cb1bf53d8c79fe5f9db47
VietnamBoy13 <alter.tom@inbox.com>
2025-05-17 15:03:28 +0400

    сделаны такие задачи как: подключение бд создание отчётов в виде html и log

---
+++ uploaded_tests/test 1.py
@@ -0,0 +1,10 @@
+import pytest
+from django.test import Client
+
+@pytest.mark.django_db
+def test_homepage_loads():
+    client = Client()
+    response = client.get('/')
+    
+    assert response.status_code == 200
+    assert b"Платформа автотестирования" in response.content  # заголовок из home.html
